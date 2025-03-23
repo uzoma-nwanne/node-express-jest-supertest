d3b4fdfa0cf3ffb24000cd3a29ae43f603fa313b
Uzoma <uzoma.nwanne@gmail.com>
2025-03-23 16:45:01 +0100

    configured with multiple environments

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,7 +1,12 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-dotenv.config()
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+
+dotenv.config({ path: __dirname + `/../../config/${process.env.NODE_ENV}.env` })
 const dbURL = process.env.MONGODB_URL;
 console.log(process.env.MONGODB_URL)
 console.log(dbURL)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import { connectDB } from './db/connection.js';
 import  app from './app.js';
 import dotenv from "dotenv";
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 
-//dotenv.config()
+dotenv.config({ path: __dirname + `/../config/${process.env.NODE_ENV}.env` })
 const port = process.env.PORT;
 
 app.listen(port, ()=>{
