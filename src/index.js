5c83983ee982493de326aa73c656c01f23b6b3ca
Uzoma <uzoma.nwanne@gmail.com>
2025-03-24 09:45:16 +0100

    edited main

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import { connectDB } from './db/connection.js';
 import  app from './app.js';
 import dotenv from "dotenv";
+import chalk from 'chalk';
 
 //dotenv.config()
 const port = process.env.PORT;
 
 app.listen(port, ()=>{
-    console.log('Server is up on Port ' + port)
+    console.log(chalk.green.inverse('Server is up on Port ' + port))
     connectDB()
 })
